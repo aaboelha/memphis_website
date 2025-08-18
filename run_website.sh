442277561d9485473f8dfd5f79fa3122b4d53cdf
ahmad <ahawadaboel@gmail.com>
2025-08-18 22:18:04 +0200

    Update the styling in ask.html and update the run script to kill the running docker image before building and re-running

--- run_website.sh
+++ run_website.sh
@@ -2,6 +2,14 @@
 #uvicorn main:app --host 0.0.0.0 --port 8000 &
 #echo "Server started with PID $!"
 
+# Kill and remove any running memphis_website container
+container_id=$(docker ps -q --filter ancestor=memphis_website)
+if [ -n "$container_id" ]; then
+  echo "Stopping and removing running memphis_website container: $container_id"
+  docker stop $container_id
+  docker rm $container_id
+fi
+
 docker build -t memphis_website .
 docker run -d -p 8070:8000 memphis_website
 
